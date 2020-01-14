81c1ab97b9452c2cfa271c404a43d9ce8ff3dc4e
Max Turgeon <turgeon.maxime@gmail.com>
2020-01-14 17:14:20 -0600

    update multivariate normal slides

--- static/slides/multivariate-normal.R
+++ static/slides/multivariate-normal.R
@@ -40,3 +40,119 @@ Y %>%
   ggplot(aes(X1, X2)) +
   geom_density_2d()
 
+
+## -----------------------------------------------------------------------------
+# Ramus data, Timm (2002)
+ramus <- read.csv("https://maxturgeon.ca/w20-stat7200/Ramus.csv")
+head(ramus)
+
+
+## -----------------------------------------------------------------------------
+var_names <- c("Age8", "Age8.5",
+               "Age9", "Age9.5")
+
+par(mfrow = c(2, 2))
+for (var in var_names) {
+  qqnorm(ramus[, var], main = var)
+  qqline(ramus[, var])
+}
+
+
+## -----------------------------------------------------------------------------
+ramus <- ramus[,var_names]
+sigma_hat <- cov(ramus)
+
+ramus_cent <- scale(ramus, center = TRUE, 
+                    scale = FALSE)
+
+D_vect <- apply(ramus_cent, 1, function(row) {
+  t(row) %*% solve(sigma_hat) %*% row
+})
+
+
+## -----------------------------------------------------------------------------
+qqplot(qchisq(ppoints(length(D_vect)), df = 4),
+       D_vect, xlab = "Theoretical Quantiles")
+qqline(D_vect, distribution = function(p) {
+  qchisq(p, df = 4)
+  })
+
+
+## -----------------------------------------------------------------------------
+library(mvtnorm)
+set.seed(123)
+
+n <- 50; p <- 2
+
+mu <- c(1, 2)
+Sigma <- matrix(c(1, 0.5, 0.5, 1), ncol = p)
+
+Y <- rmvnorm(n, mean = mu, sigma = Sigma)
+
+
+## -----------------------------------------------------------------------------
+loglik <- function(mu, sigma, data = Y) {
+  # Compute quantities
+  y_bar <- colMeans(Y)
+  quad_form <- t(y_bar - mu) %*% solve(sigma) %*%
+    (y_bar - mu)
+  
+  -0.5*n*log(det(sigma)) -
+    0.5*(n - 1)*sum(diag(solve(sigma) %*% cov(Y))) -
+    0.5*n*drop(quad_form)
+}
+
+
+## -----------------------------------------------------------------------------
+grid_xy <- expand.grid(seq(0, 2, length.out = 32), 
+                       seq(0, 4, length.out = 32))
+
+head(grid_xy, n = 5)
+
+
+## -----------------------------------------------------------------------------
+contours <- purrr::map_df(seq_len(nrow(grid_xy)), 
+                          function(i) {
+  # Where we will evaluate loglik
+  mu_obs <- as.numeric(grid_xy[i,])
+  # Evaluate at the pop covariance
+  z <- loglik(mu_obs, sigma = Sigma)
+  # Output data.frame
+  data.frame(x = mu_obs[1],
+             y = mu_obs[2],
+             z = z)
+})
+
+
+## ---- message = FALSE---------------------------------------------------------
+library(tidyverse)
+library(ggrepel)
+# Create df with pop and sample means
+data_means <- data.frame(x = c(mu[1], mean(Y[,1])),
+                         y = c(mu[2], mean(Y[,2])),
+                         label = c("Pop.", "Sample"))
+
+
+## ---- message = FALSE---------------------------------------------------------
+ggplot(contours, aes(x, y)) + 
+  geom_contour(aes(z = z)) + 
+  geom_point(data = data_means) +
+  geom_label_repel(data = data_means,
+                   aes(label = label))
+
+
+## -----------------------------------------------------------------------------
+library(scatterplot3d)
+with(contours, scatterplot3d(x, y, z))
+
+
+## ---- echo = FALSE, eval = FALSE----------------------------------------------
+## x <- seq(0, 2, length.out = 32)
+## y <- seq(0, 4, length.out = 32)
+## z <- matrix(NA, ncol = 32, nrow = 32)
+## for (i in seq_len(32)) {
+##   for (j in seq_len(32))
+##     z[i,j] <- loglik(c(x[i], y[j]), sigma = Sigma)
+## }
+## persp(x, y, z, theta = 30, phi = 30, expand = 0.5, col = "lightblue")
+
